e441b5e4426189a8d4bd49c2e9f5eb4c83ffbeba
Wanchana <s.wanchana41@gmail.com>
2024-06-05 14:06:06 +0700

    feat: add array object as postItems, refactor post.jsx by using map to show each post from postItems, reassign initial value of state to likes of each post sorting by id

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,46 +1,129 @@
 import { useState } from "react";
 
-function Posts() {
-  const [likeCount, setLikeCount] = useState(10);
+const postItems = [
+  {
+    id: 1,
+    title: "Paper Clips",
+    content:
+      "Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum.",
+    likes: 61,
+  },
+  {
+    id: 2,
+    title: "Born to Kill",
+    content:
+      "Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla.",
+    likes: 46,
+  },
+  {
+    id: 3,
+    title: "Ten Shrews, The (Kymmenen riivinrautaa)",
+    content:
+      "Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.",
+    likes: 50,
+  },
+  {
+    id: 4,
+    title: "Ciel est Ã  vous, Le (Woman Who Dared, The)",
+    content: "Maecenas ut massa quis augue luctus tincidunt.",
+    likes: 3,
+  },
+  {
+    id: 5,
+    title: "Tigger Movie, The",
+    content: "Aenean sit amet justo.",
+    likes: 66,
+  },
+  {
+    id: 6,
+    title: "Cat People",
+    content:
+      "Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus. Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
+    likes: 70,
+  },
+  {
+    id: 7,
+    title: "Brooklyn Castle",
+    content:
+      "In hac habitasse platea dictumst. Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh.",
+    likes: 46,
+  },
+  {
+    id: 8,
+    title: "Snow Angels",
+    content:
+      "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat.",
+    likes: 1,
+  },
+  {
+    id: 9,
+    title: "Rose Tattoo, The",
+    content: "Curabitur in libero ut massa volutpat convallis.",
+    likes: 20,
+  },
+  {
+    id: 10,
+    title: "It's A Wonderful World",
+    content:
+      "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien.",
+    likes: 6,
+  },
+];
 
-  const handleLikeClick = () => {
-    setLikeCount(likeCount + 1);
+function Posts() {
+  const [postLikes, setPostLikes] = useState(
+    postItems.reduce((acc, item) => {
+      acc[item.id] = item.likes;
+      return acc;
+    }, {})
+  );
+
+  const handleLikeClick = (postID) => {
+    setPostLikes({
+      ...postLikes,
+      [postID]: postLikes[postID] + 1,
+    });
   };
 
-  const handleDislikeClick = () => {
-    if (likeCount > 0) {
-      setLikeCount(likeCount - 1);
+  const handleDislikeClick = (postID) => {
+    if (postLikes[postID] > 0) {
+      setPostLikes({
+        ...postLikes,
+        [postID]: postLikes[postID] - 1,
+      });
     }
   };
 
   return (
-    <div class="app-wrapper">
-      <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">{likeCount}</span>
+    <div className="app-wrapper">
+      <h1 className="app-title">Posts</h1>
+      <div className="post-list">
+        {postItems.map(({ id, title, content }) => (
+          <div key={id} className="post-item">
+            <div className="post-header">
+              <h2>{title}</h2>
+              <div className="post-social-media-stats">
+                <span className="stats-topic">Likes: </span>
+                <span className="post-likes">{postLikes[id]}</span>
+              </div>
+            </div>
+            <p className="post-content">{content}</p>
+            <div className="post-actions">
+              <button
+                className="like-button"
+                onClick={() => handleLikeClick(id)}
+              >
+                Like
+              </button>
+              <button
+                className="dislike-button"
+                onClick={() => handleDislikeClick(id)}
+              >
+                Dislike
+              </button>
             </div>
           </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button" onClick={handleLikeClick}>
-              Like
-            </button>
-            <button class="dislike-button" onClick={handleDislikeClick}>
-              Dislike
-            </button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
